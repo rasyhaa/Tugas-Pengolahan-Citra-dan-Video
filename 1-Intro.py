d1b16d3e86a9e9a98e4cd949ba568f9cc9bb143b
Rasyha Syauqi <rasyhasyauqi@gmail.com>
2026-09-08 21:00:20 +0700

    Combine image and webcam filters into 1-intro

---
+++ 1-Intro.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+# FILTER GAMBAR
+img = cv2.imread("gambarcontoh.webp")
+
+# Membuat gambar
+red = np.zeros_like(img)
+green = np.zeros_like(img)
+blue = np.zeros_like(img)
+
+# Filter
+red[:, :, 2] = img[:, :, 2]
+green[:, :, 1] = img[:, :, 1]
+blue[:, :, 0] = img[:, :, 0]
+
+# Grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Hasil
+cv2.imshow("Red", red)
+cv2.imshow("Green", green)
+cv2.imshow("Blue", blue)
+cv2.imshow("Grayscale", gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# FILTER WEBCAM
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Membuat gambar
+    filter = np.zeros_like(frame)
+
+    # Filter 0 = biru, 1 = hijau, 2 = merah
+    filter[:, :, 2] = frame[:, :, 2]
+
+    # Filter
+    cv2.imshow("Filter", filter)
+
+    # Tekan ESC untuk keluar
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
